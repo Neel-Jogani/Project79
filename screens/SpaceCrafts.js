df236f5651bc6f95bf762f0f476b379c20b16259
Neel-Jogani <73420273+Neel-Jogani@users.noreply.github.com>
2021-08-08 11:38:43 +0530

    Add files via upload

---
+++ screens/SpaceCrafts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { ViewBase,Text, TouchableOpacity, Platform } from 'react-native';
+
+export default class SpaceCrafts extends Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Space Crafts Screen
+                </Text>
+            </View>
+        )
+    }
+}
